3bcfb78d21b0a8ce7d81bf7a7177aa59f7d5da41
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-31 20:46:58 +0700

    created layout cell

---
+++ GameCatalogue/Controller/GameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameTableViewCell.swift
+//  GameCatalogue
+//
+//  Created by Wahid Hidayat on 31/01/21.
+//  Copyright © 2021 Wakhid Saiful Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
