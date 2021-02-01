7998a60dfccc974f5e7d38cacb7ae090ff7f6e96
Wahid Hidayat <wshidayat38@gmail.com>
2021-02-01 13:39:34 +0700

    added xib layout for detail game

---
+++ GameCatalogue/Controller/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  GameCatalogue
+//
+//  Created by Wahid Hidayat on 01/02/21.
+//  Copyright © 2021 Wakhid Saiful Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- GameCatalogue/Model/GameDetail.swift
+++ GameCatalogue/Model/GameDetail.swift
@@ -1,27 +1,30 @@
 //
-//  Game.swift
+//  GameDetail.swift
 //  GameCatalogue
 //
-//  Created by Wahid Hidayat on 31/01/21.
+//  Created by Wahid Hidayat on 01/02/21.
 //  Copyright © 2021 Wakhid Saiful Hidayat. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-struct GameResult: Codable {
-    let games: [Game]
-    
-    enum CodingKeys: String, CodingKey {
-        case games = "results"
-    }
-}
-
-struct Game: Codable {
+struct GameDetail: Codable {
     let id: Int
     let name: String
+    let released: String?
     let backgroundImage: String?
     let rating: Double
+    let description: String
+    let platforms: [Platform]
     let genres: [Genre]
+    
+    struct Platform: Codable {
+        let platform: PlatformName
+        
+        struct PlatformName: Codable {
+            let name: String
+        }
+    }
     
     struct Genre: Codable {
         let name: String
@@ -30,8 +33,11 @@ struct Game: Codable {
     enum CodingKeys: String, CodingKey {
         case id
         case name
+        case released
         case backgroundImage = "background_image"
         case rating
+        case description
+        case platforms
         case genres
     }
 }
